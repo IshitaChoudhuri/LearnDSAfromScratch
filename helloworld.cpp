aa58c493c80aab3982b48db3988667767e351d07
Ishita Choudhuri <92997901+IshitaChoudhuri@users.noreply.github.com>
2024-06-08 15:34:35 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a =5;
+	char b='c';
+    cout<<"Hello World"<<endl;
+    cout<<a<<b<<endl;
+    cout<<sizeof(b)<<endl;
+
+	int a1 =5;
+	float b1=2;
+	cout<<(b/a)<<endl;
+	char ch=123456;
+	cout<<ch<<endl;
+	
+	int x=2;
+	int y=5;
+	
+	bool first=(x==y);
+	cout<<first<<endl;
+	
+	bool second=(x>=y);
+	cout<<second<<endl;
+	
+	bool third=(x<=y);
+	cout<<third<<endl;
+	
+	bool fourth=(x>y);
+	cout<<fourth<<endl;
+	
+	bool fifth=(x<y);
+	cout<<fifth<<endl;
+	
+	bool sixth=(x!=y);
+	cout<<sixth<<endl;
+}
+
