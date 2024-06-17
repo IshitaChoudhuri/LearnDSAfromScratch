b7010f761355ec00321e61f9f020a5bd4a0ce18b
Ishita Choudhuri <92997901+IshitaChoudhuri@users.noreply.github.com>
2024-06-17 18:49:47 +0530

    Add files via upload

---
+++ maxMin.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int getMin(int num[], int n) {
+
+    int mini = INT_MAX;
+    
+    for(int i = 0; i<n; i++) {
+        
+        mini = min( mini, num[i]);
+        
+	}
+
+    return mini;
+}
+
+int getMax(int num[], int n) {
+
+    int maxi = INT_MIN;
+    
+    for(int i = 0; i<n; i++) {
+
+        maxi = max(maxi, num[i]);
+    }
+
+    return maxi;
+}
+
+int main() {
+
+    int size;
+    cout<< "Enter the number of values: ";
+    cin >> size;
+
+    int num[100];
+
+	cout<<"Enter the values: "<<endl;
+    for(int i = 0; i<size; i++) {
+        cin >> num[i];
+    }
+
+    cout << "Maximum value is " << getMax(num, size) << endl;
+    cout << "Minimum value is " << getMin(num, size) << endl;
+
+
+    return 0;
+}
